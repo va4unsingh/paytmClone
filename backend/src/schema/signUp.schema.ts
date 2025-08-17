9372add93a489b903ce2e1a809f86f693cf022a9
Varun Singh <varunsingh7004@gmail.com>
2025-08-18 01:11:10 +0530

    Auth works fine

--- backend/src/schema/signUp.schema.ts
+++ backend/src/schema/signUp.schema.ts
@@ -5,7 +5,6 @@ export const signUpBody = zod.object({
     .string()
     .min(2, "Username must be atleast 2 characters")
     .max(20, "Username must be no more than 20 characters")
-    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
     .toLowerCase(),
   password: z
     .string()
